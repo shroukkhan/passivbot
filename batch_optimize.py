8e64ecbc19a641792640d719b507d72720cfb1f4
Shrouk Khan <shrouk.khan@agoda.com>
2022-01-22 15:30:10 +0700

    Harmony search is not working for 10 cpus :/

---
+++ batch_optimize.py
@@ -0,0 +1,42 @@
+import os
+import shutil
+import subprocess
+
+from procedures import make_get_filepath
+
+
+def main():
+    tokens = ['ADA', 'ATOM', 'AXS', 'DASH', 'DYDX', 'LINK', 'MANA', 'MATIC', 'OMG', 'SOL', 'XLM', 'XTZ']
+    start_from = 'ADA'
+    symbols = tokens[tokens.index(start_from):] + tokens[:tokens.index(start_from)]
+
+    quote = 'USDT'
+    cfgs_dir = make_get_filepath('cfgs_batch_optimize/')
+    exchange = 'binance'
+
+    symbols = [e + quote for e in symbols]
+    kwargs_list = [{
+        'start': cfgs_dir,
+        'symbol': symbol,
+        'starting_balance': 60.0,
+        'start_date': '2021-07-01',
+        'end_date': '2022-01-15',
+    } for symbol in symbols]
+    for kwargs in kwargs_list:
+        formatted = f"python3 harmony_search.py "
+        for key in kwargs:
+            formatted += f' --{key} {kwargs[key]}'
+        print(formatted)
+        subprocess.run([formatted], shell=True)
+        try:
+            d = f'backtests/{exchange}/{kwargs["symbol"]}/plots/'
+            ds = sorted([f for f in os.listdir(d) if '20' in f])
+            for d1 in ds:
+                print(f'copying resulting config to {cfgs_dir}', d + d1)
+                shutil.copy(d + d1 + '/live_config.json', f'{cfgs_dir}{kwargs["symbol"]}_{d1}.json')
+        except Exception as e:
+            print('error', kwargs['symbol'], e)
+
+
+if __name__ == '__main__':
+    main()
