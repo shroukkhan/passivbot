385600f2c6395fdc730185a9c699c27a60482658
Shrouk Khan <shrouk.khan@agoda.com>
2022-01-28 23:28:16 +0700

    incomplete result analyzer added

--- batch_optimize.bat
+++ batch_optimize.bat
@@ -1,13 +1,12 @@
 @echo off
 for %%x in (
-        MATICUSDT
-        DOTUSDT
-        SANDUSDT
-        FTMUSDT
-        LTCUSDT
-        LINKUSDT
-        MANAUSDT
         ETCUSDT
+        EOSUSDT
+        ATOMUSDT
+        FILUSDT
+        ICPUSDT
+        ALICEUSDT
+        LRCUSDT
        ) do (
          echo Doing python harmony_search.py  --symbol %%x --end_date 2022-01-27 --n_cpus 12
          python harmony_search.py  --symbol %%x --end_date 2022-01-27 --n_cpus 12
