d951d5ca71ac7dafa9d38da4e6fdb9e01e20f366
Shrouk Khan <shrouk.khan@agoda.com>
2022-02-16 16:29:30 +0700

    change some harmony search param, before merging with master

--- batch_optimize.bat
+++ batch_optimize.bat
@@ -1,16 +1,22 @@
 @echo off
 for %%x in (
-        RUNEUSDT
-        OMGUSDT
-        ENJUSDT
-        CELRUSDT
-        SXPUSDT
-        1INCHUSDT
-        SRMUSDT
-        COTIUSDT
+        ADAUSDT
+        ALGOUSDT
+        ALICEUSDT
+        ATOMUSDT
+        CHZUSDT
+        CRVUSDT
+        ETCUSDT
+        FTMUSDT
+        ICPUSDT
+        LINKUSDT
+        LTCUSDT
+        MATICUSDT
+        ONEUSDT
+        XRPUSDT
        ) do (
          echo Doing python harmony_search.py  --symbol %%x --end_date 2022-02-02 --n_cpus 12
-         python harmony_search.py  --symbol %%x --end_date 2022-02-02 --n_cpus 12
+         python harmony_search.py  --symbol %%x --start_date 2021-09-01 --end_date 2022-02-15 --n_cpus 12
          echo.
          echo =-=-=-=-=-=
          echo.
