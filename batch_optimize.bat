246cf586e5aecd3e8eff2bbf5075e275793238a9
Shrouk Khan <shrouk.khan@agoda.com>
2022-02-16 16:41:17 +0700

    update batch_optimize.bat , change start date

--- batch_optimize.bat
+++ batch_optimize.bat
@@ -16,7 +16,7 @@ for %%x in (
         XRPUSDT
        ) do (
          echo Doing python harmony_search.py  --symbol %%x --end_date 2022-02-02 --n_cpus 12
-         python harmony_search.py  --symbol %%x --start_date 2021-09-01 --end_date 2022-02-15 --n_cpus 12
+         python harmony_search.py  --symbol %%x --start_date 2021-08-01 --end_date 2022-02-15 --n_cpus 12
          echo.
          echo =-=-=-=-=-=
          echo.
