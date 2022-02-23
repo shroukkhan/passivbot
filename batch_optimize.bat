92c8a6a3e697c47b4fcf2bf402abe908598a36a0
Shrouk Khan <shrouk.khan@agoda.com>
2022-02-24 06:15:45 +0700

    Update some live config

--- batch_optimize.bat
+++ batch_optimize.bat
@@ -1,14 +1,9 @@
 @echo off
 for %%x in (
-         QTUMUSDT
-         GRTUSDT
-         DENTUSDT
-         SFPUSDT
-         STORJUSDT
-         IOTAUSDT
-         HOTUSDT
-         NEOUSDT
-         COMPUSDT
+         ONEUSDT
+         FTMUSDT
+         CHZUSDT
+         ICPUSDT
        ) do (
          echo harmony_search.py --symbol %%x --start_date 2021-08-01 --end_date 2022-02-15 --n_cpus 12
          python harmony_search.py  --symbol %%x --start_date 2021-08-01 --end_date 2022-02-15 --n_cpus 12
