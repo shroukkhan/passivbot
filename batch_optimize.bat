51a5df6dc4704c91fccd1e608ce3b360c09baa0b
Shrouk Khan <shrouk.khan@agoda.com>
2022-02-25 21:30:13 +0700

    before moving to 5.5

--- batch_optimize.bat
+++ batch_optimize.bat
@@ -1,10 +1,11 @@
 @echo off
 for %%x in (
-         ALPHAUSDT
-         RLCUSDT
-         HBARUSDT
-         DODOUSDT
-         XMRUSDT
+         IOTAUSDT
+         COTIUSDT
+         LINKUSDT
+         ATOMUSDT
+         ALGOUSDT
+
        ) do (
          echo harmony_search.py --symbol %%x --start_date 2021-08-01 --end_date 2022-02-15 --n_cpus 12
          python harmony_search.py  --symbol %%x --start_date 2021-08-01 --end_date 2022-02-15 --n_cpus 12
