7b3888e3ce87fcab653470b112d1b2dfb5f4eace
Shrouk Khan <shrouk.khan@agoda.com>
2022-03-02 06:51:29 +0700

    before adding more optimizer

--- batch_optimize.bat
+++ batch_optimize.bat
@@ -1,14 +1,9 @@
 @echo off
 for %%x in (
-           NEOUSDT
-           XLMUSDT
-           HBARUSDT
-           SXPUSDT
-           ETHUSDT
-           XRPUSDT
-           1000SHIBUSDT
-           DOGEUSDT
-           ADAUSDT
+           FTMUSDT
+           ONEUSDT
+           COTIUSDT
+           1INCHUSDT
        ) do (
          ::echo harmony_search.py  -oh --symbol %%x -pm r --start_date 2021-08-01 --end_date 2022-02-25 --n_cpus 12
          ::python harmony_search.py --symbol %%x -pm r --start_date 2021-08-01 --end_date 2022-02-25 --n_cpus 6
