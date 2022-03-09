d95a06d4442a27c26c99279d2d53e98a51efeaf1
Shrouk Khan <shrouk.khan@agoda.com>
2022-03-10 06:55:36 +0700

    Added more config

--- start.sh
+++ start.sh
@@ -1,361 +1,361 @@
 screen -X -S ALICE kill
-screen -X -S ALICE_5_4 kill
-screen -X -S ALICE_5_5 kill
+screen -X -S ALICE_binance kill
+screen -X -S ALICE_binance kill
 screen -dmS ALICE_binance  ./run.sh ALICE
 
 screen -X -S GRT kill
-screen -X -S GRT_5_4 kill
-screen -X -S GRT_5_5 kill
+screen -X -S GRT_binance kill
+screen -X -S GRT_binance kill
 screen -dmS GRT_binance  ./run.sh GRT
 
 screen -X -S IOST kill
-screen -X -S IOST_5_4 kill
-screen -X -S IOST_5_5 kill
+screen -X -S IOST_binance kill
+screen -X -S IOST_binance kill
 screen -dmS IOST_binance  ./run.sh IOST
 
 screen -X -S OMG kill
-screen -X -S OMG_5_4 kill
-screen -X -S OMG_5_5 kill
+screen -X -S OMG_binance kill
+screen -X -S OMG_binance kill
 screen -dmS OMG_binance  ./run.sh OMG
 
 screen -X -S ETC kill
-screen -X -S ETC_5_4 kill
-screen -X -S ETC_5_5 kill
+screen -X -S ETC_binance kill
+screen -X -S ETC_binance kill
 screen -dmS ETC_binance  ./run.sh ETC
 
 screen -X -S ATOM kill
-screen -X -S ATOM_5_4 kill
-screen -X -S ATOM_5_5 kill
+screen -X -S ATOM_binance kill
+screen -X -S ATOM_binance kill
 screen -dmS ATOM_binance  ./run.sh ATOM
 
 screen -X -S KAVA kill
-screen -X -S KAVA_5_4 kill
-screen -X -S KAVA_5_5 kill
+screen -X -S KAVA_binance kill
+screen -X -S KAVA_binance kill
 screen -dmS KAVA_binance  ./run.sh KAVA
 
 screen -X -S COTI kill
-screen -X -S COTI_5_4 kill
-screen -X -S COTI_5_5 kill
+screen -X -S COTI_binance kill
+screen -X -S COTI_binance kill
 screen -dmS COTI_binance  ./run.sh COTI
 
 screen -X -S LTC kill
-screen -X -S LTC_5_4 kill
-screen -X -S LTC_5_5 kill
+screen -X -S LTC_binance kill
+screen -X -S LTC_binance kill
 screen -dmS LTC_binance  ./run.sh LTC
 
 screen -X -S ONE kill
-screen -X -S ONE_5_4 kill
-screen -X -S ONE_5_5 kill
+screen -X -S ONE_binance kill
+screen -X -S ONE_binance kill
 screen -dmS ONE_binance  ./run.sh ONE
 
 screen -X -S CHZ kill
-screen -X -S CHZ_5_4 kill
-screen -X -S CHZ_5_5 kill
+screen -X -S CHZ_binance kill
+screen -X -S CHZ_binance kill
 screen -dmS CHZ_binance  ./run.sh CHZ
 
 screen -X -S NEO kill
-screen -X -S NEO_5_4 kill
-screen -X -S NEO_5_5 kill
+screen -X -S NEO_binance kill
+screen -X -S NEO_binance kill
 screen -dmS NEO_binance  ./run.sh NEO
 
 screen -X -S ADA kill
-screen -X -S ADA_5_4 kill
-screen -X -S ADA_5_5 kill
+screen -X -S ADA_binance kill
+screen -X -S ADA_binance kill
 screen -dmS ADA_binance  ./run.sh ADA
 
 screen -X -S DOGE kill
-screen -X -S DOGE_5_4 kill
-screen -X -S DOGE_5_5 kill
+screen -X -S DOGE_binance kill
+screen -X -S DOGE_binance kill
 screen -dmS DOGE_binance  ./run.sh DOGE
 
 screen -X -S HBAR kill
-screen -X -S HBAR_5_4 kill
-screen -X -S HBAR_5_5 kill
+screen -X -S HBAR_binance kill
+screen -X -S HBAR_binance kill
 screen -dmS HBAR_binance  ./run.sh HBAR
 
 screen -X -S SXP kill
-screen -X -S SXP_5_4 kill
-screen -X -S SXP_5_5 kill
+screen -X -S SXP_binance kill
+screen -X -S SXP_binance kill
 screen -dmS SXP_binance  ./run.sh SXP
 
 screen -X -S XLM kill
-screen -X -S XLM_5_4 kill
-screen -X -S XLM_5_5 kill
+screen -X -S XLM_binance kill
+screen -X -S XLM_binance kill
 screen -dmS XLM_binance  ./run.sh XLM
 
 screen -X -S XRP kill
-screen -X -S XRP_5_4 kill
-screen -X -S XRP_5_5 kill
+screen -X -S XRP_binance kill
+screen -X -S XRP_binance kill
 screen -dmS XRP_binance  ./run.sh XRP
 
 screen -X -S FTM kill
-screen -X -S FTM_5_4 kill
-screen -X -S FTM_5_5 kill
+screen -X -S FTM_binance kill
+screen -X -S FTM_binance kill
 
 screen -X -S 1INCH kill
-screen -X -S 1INCH_5_4 kill
-screen -X -S 1INCH_5_5 kill
+screen -X -S 1INCH_binance kill
+screen -X -S 1INCH_binance kill
 screen -dmS 1INCH_binance  ./run.sh 1INCH
 
 screen -X -S RLC kill
-screen -X -S RLC_5_4 kill
-screen -X -S RLC_5_5 kill
+screen -X -S RLC_binance kill
+screen -X -S RLC_binance kill
 screen -dmS RLC_binance  ./run.sh RLC
 
 screen -X -S SUSHI kill
-screen -X -S SUSHI_5_4 kill
-screen -X -S SUSHI_5_5 kill
+screen -X -S SUSHI_binance kill
+screen -X -S SUSHI_binance kill
 screen -dmS SUSHI_binance  ./run.sh SUSHI
 
 screen -X -S WAVES kill
-screen -X -S WAVES_5_4 kill
-screen -X -S WAVES_5_5 kill
+screen -X -S WAVES_binance kill
+screen -X -S WAVES_binance kill
 screen -dmS WAVES_binance  ./run.sh WAVES
 
 screen -X -S ALPHA kill
-screen -X -S ALPHA_5_4 kill
-screen -X -S ALPHA_5_5 kill
+screen -X -S ALPHA_binance kill
+screen -X -S ALPHA_binance kill
 screen -dmS ALPHA_binance  ./run.sh ALPHA
 
 screen -X -S COMP kill
-screen -X -S COMP_5_4 kill
-screen -X -S COMP_5_5 kill
+screen -X -S COMP_binance kill
+screen -X -S COMP_binance kill
 screen -dmS COMP_binance  ./run.sh COMP
 
 screen -X -S LINA kill
-screen -X -S LINA_5_4 kill
-screen -X -S LINA_5_5 kill
+screen -X -S LINA_binance kill
+screen -X -S LINA_binance kill
 screen -dmS LINA_binance  ./run.sh LINA
 
 screen -X -S QTUM kill
-screen -X -S QTUM_5_4 kill
-screen -X -S QTUM_5_5 kill
+screen -X -S QTUM_binance kill
+screen -X -S QTUM_binance kill
 screen -dmS QTUM_binance  ./run.sh QTUM
 
 screen -X -S ALGO kill
-screen -X -S ALGO_5_4 kill
-screen -X -S ALGO_5_5 kill
+screen -X -S ALGO_binance kill
+screen -X -S ALGO_binance kill
 screen -dmS ALGO_binance  ./run.sh ALGO
 
 screen -X -S CRV kill
-screen -X -S CRV_5_4 kill
-screen -X -S CRV_5_5 kill
+screen -X -S CRV_binance kill
+screen -X -S CRV_binance kill
 screen -dmS CRV_binance  ./run.sh CRV
 
 screen -X -S ENJ kill
-screen -X -S ENJ_5_4 kill
-screen -X -S ENJ_5_5 kill
+screen -X -S ENJ_binance kill
+screen -X -S ENJ_binance kill
 screen -dmS ENJ_binance  ./run.sh ENJ
 
 screen -X -S LINK kill
-screen -X -S LINK_5_4 kill
-screen -X -S LINK_5_5 kill
+screen -X -S LINK_binance kill
+screen -X -S LINK_binance kill
 screen -dmS LINK_binance  ./run.sh LINK
 
 screen -X -S MATIC kill
-screen -X -S MATIC_5_4 kill
-screen -X -S MATIC_5_5 kill
+screen -X -S MATIC_binance kill
+screen -X -S MATIC_binance kill
 screen -dmS MATIC_binance  ./run.sh MATIC
 
 
 screen -X -S RUNE kill
-screen -X -S RUNE_5_4 kill
-screen -X -S RUNE_5_5 kill
+screen -X -S RUNE_binance kill
+screen -X -S RUNE_binance kill
 screen -dmS RUNE_binance  ./run.sh RUNE
 
 screen -X -S SFP kill
-screen -X -S SFP_5_4 kill
-screen -X -S SFP_5_5 kill
+screen -X -S SFP_binance kill
+screen -X -S SFP_binance kill
 screen -dmS SFP_binance  ./run.sh SFP
 
 screen -X -S VET kill
-screen -X -S VET_5_4 kill
-screen -X -S VET_5_5 kill
+screen -X -S VET_binance kill
+screen -X -S VET_binance kill
 screen -dmS VET_binance  ./run.sh VET
 
 screen -X -S XMR kill
-screen -X -S XMR_5_4 kill
-screen -X -S XMR_5_5 kill
+screen -X -S XMR_binance kill
+screen -X -S XMR_binance kill
 screen -dmS XMR_binance  ./run.sh XMR
 
 
 screen -X -S REN kill
-screen -X -S REN_5_4 kill
-screen -X -S REN_5_5 kill
+screen -X -S REN_binance kill
+screen -X -S REN_binance kill
 screen -dmS REN_binance  ./run.sh REN
 
 
 screen -X -S ICX kill
-screen -X -S ICX_5_4 kill
-screen -X -S ICX_5_5 kill
+screen -X -S ICX_binance kill
+screen -X -S ICX_binance kill
 screen -dmS ICX_binance  ./run.sh ICX
 
 
 screen -X -S HBAR kill
-screen -X -S HBAR_5_4 kill
-screen -X -S HBAR_5_5 kill
+screen -X -S HBAR_binance kill
+screen -X -S HBAR_binance kill
 screen -dmS HBAR_binance  ./run.sh HBAR
 
 
 screen -X -S DODO kill
-screen -X -S DODO_5_4 kill
-screen -X -S DODO_5_5 kill
+screen -X -S DODO_binance kill
+screen -X -S DODO_binance kill
 screen -dmS DODO_binance  ./run.sh DODO
 
 
 screen -X -S DENT kill
-screen -X -S DENT_5_4 kill
-screen -X -S DENT_5_5 kill
+screen -X -S DENT_binance kill
+screen -X -S DENT_binance kill
 screen -dmS DENT_binance  ./run.sh DENT
 
 screen -X -S HOT kill
-screen -X -S HOT_5_4 kill
-screen -X -S HOT_5_5 kill
+screen -X -S HOT_binance kill
+screen -X -S HOT_binance kill
 screen -dmS HOT_binance  ./run.sh HOT
 
 screen -X -S ICP kill
-screen -X -S ICP_5_4 kill
-screen -X -S ICP_5_5 kill
+screen -X -S ICP_binance kill
+screen -X -S ICP_binance kill
 screen -dmS ICP_binance  ./run.sh ICP
 
 screen -X -S IOTA kill
-screen -X -S IOTA_5_4 kill
-screen -X -S IOTA_5_5 kill
+screen -X -S IOTA_binance kill
+screen -X -S IOTA_binance kill
 screen -dmS IOTA_binance  ./run.sh IOTA
 
 screen -X -S SNX kill
-screen -X -S SNX_5_4 kill
-screen -X -S SNX_5_5 kill
+screen -X -S SNX_binance kill
+screen -X -S SNX_binance kill
 screen -dmS SNX_binance  ./run.sh SNX
 
 screen -X -S ANKR kill
-screen -X -S ANKR_5_4 kill
-screen -X -S ANKR_5_5 kill
+screen -X -S ANKR_binance kill
+screen -X -S ANKR_binance kill
 screen -dmS ANKR_binance  ./run.sh ANKR
 
 screen -X -S BEL kill
-screen -X -S BEL_5_4 kill
-screen -X -S BEL_5_5 kill
+screen -X -S BEL_binance kill
+screen -X -S BEL_binance kill
 screen -dmS BEL_binance  ./run.sh BEL
 
 screen -X -S NKN kill
-screen -X -S NKN_5_4 kill
-screen -X -S NKN_5_5 kill
+screen -X -S NKN_binance kill
+screen -X -S NKN_binance kill
 screen -dmS NKN_binance  ./run.sh NKN
 
 screen -X -S ONT kill
-screen -X -S ONT_5_4 kill
-screen -X -S ONT_5_5 kill
+screen -X -S ONT_binance kill
+screen -X -S ONT_binance kill
 screen -dmS ONT_binance  ./run.sh ONT
 
 screen -X -S REEF kill
-screen -X -S REEF_5_4 kill
-screen -X -S REEF_5_5 kill
+screen -X -S REEF_binance kill
+screen -X -S REEF_binance kill
 screen -dmS REEF_binance  ./run.sh REEF
 
 screen -X -S ZEN kill
-screen -X -S ZEN_5_4 kill
-screen -X -S ZEN_5_5 kill
+screen -X -S ZEN_binance kill
+screen -X -S ZEN_binance kill
 screen -dmS ZEN_binance  ./run.sh ZEN
 
 screen -X -S BLZ kill
-screen -X -S BLZ_5_4 kill
-screen -X -S BLZ_5_5 kill
+screen -X -S BLZ_binance kill
+screen -X -S BLZ_binance kill
 screen -dmS BLZ_binance  ./run.sh BLZ
 
 screen -X -S OGN kill
-screen -X -S OGN_5_4 kill
-screen -X -S OGN_5_5 kill
+screen -X -S OGN_binance kill
+screen -X -S OGN_binance kill
 screen -dmS OGN_binance  ./run.sh OGN
 
 screen -X -S MTL kill
-screen -X -S MTL_5_4 kill
-screen -X -S MTL_5_5 kill
+screen -X -S MTL_binance kill
+screen -X -S MTL_binance kill
 screen -dmS MTL_binance  ./run.sh MTL
 
 screen -X -S LIT kill
-screen -X -S LIT_5_4 kill
-screen -X -S LIT_5_5 kill
+screen -X -S LIT_binance kill
+screen -X -S LIT_binance kill
 screen -dmS LIT_binance  ./run.sh LIT
 
 screen -X -S RVN kill
-screen -X -S RVN_5_4 kill
-screen -X -S RVN_5_5 kill
+screen -X -S RVN_binance kill
+screen -X -S RVN_binance kill
 screen -dmS RVN_binance  ./run.sh RVN
 
 screen -X -S MKR kill
-screen -X -S MKR_5_4 kill
-screen -X -S MKR_5_5 kill
+screen -X -S MKR_binance kill
+screen -X -S MKR_binance kill
 screen -dmS MKR_binance  ./run.sh MKR
 
 screen -X -S OCEAN kill
-screen -X -S OCEAN_5_4 kill
-screen -X -S OCEAN_5_5 kill
+screen -X -S OCEAN_binance kill
+screen -X -S OCEAN_binance kill
 screen -dmS OCEAN_binance  ./run.sh OCEAN
 
 screen -X -S ZIL kill
-screen -X -S ZIL_5_4 kill
-screen -X -S ZIL_5_5 kill
+screen -X -S ZIL_binance kill
+screen -X -S ZIL_binance kill
 screen -dmS ZIL_binance  ./run.sh ZIL
 
 screen -X -S UNFI kill
-screen -X -S UNFI_5_4 kill
-screen -X -S UNFI_5_5 kill
+screen -X -S UNFI_binance kill
+screen -X -S UNFI_binance kill
 screen -dmS UNFI_binance  ./run.sh UNFI
 
 screen -X -S BAND kill
-screen -X -S BAND_5_4 kill
-screen -X -S BAND_5_5 kill
+screen -X -S BAND_binance kill
+screen -X -S BAND_binance kill
 screen -dmS BAND_binance  ./run.sh BAND
 
 screen -X -S KNC kill
-screen -X -S KNC_5_4 kill
-screen -X -S KNC_5_5 kill
+screen -X -S KNC_binance kill
+screen -X -S KNC_binance kill
 screen -dmS KNC_binance  ./run.sh KNC
 
 screen -X -S STMX kill
-screen -X -S STMX_5_4 kill
-screen -X -S STMX_5_5 kill
+screen -X -S STMX_binance kill
+screen -X -S STMX_binance kill
 screen -dmS STMX_binance  ./run.sh STMX
 
 screen -X -S TOMO kill
-screen -X -S TOMO_5_4 kill
-screen -X -S TOMO_5_5 kill
+screen -X -S TOMO_binance kill
+screen -X -S TOMO_binance kill
 screen -dmS TOMO_binance  ./run.sh TOMO
 
 screen -X -S TRB kill
-screen -X -S TRB_5_4 kill
-screen -X -S TRB_5_5 kill
+screen -X -S TRB_binance kill
+screen -X -S TRB_binance kill
 screen -dmS TRB_binance  ./run.sh TRB
 
 screen -X -S AKRO kill
-screen -X -S AKRO_5_4 kill
-screen -X -S AKRO_5_5 kill
+screen -X -S AKRO_binance kill
+screen -X -S AKRO_binance kill
 screen -dmS AKRO_binance  ./run.sh AKRO
 
 screen -X -S SC kill
-screen -X -S SC_5_4 kill
-screen -X -S SC_5_5 kill
+screen -X -S SC_binance kill
+screen -X -S SC_binance kill
 screen -dmS SC_binance  ./run.sh SC
 
 screen -X -S ZRX kill
-screen -X -S ZRX_5_4 kill
-screen -X -S ZRX_5_5 kill
+screen -X -S ZRX_binance kill
+screen -X -S ZRX_binance kill
 screen -dmS ZRX_binance  ./run.sh ZRX
 
 screen -X -S BTS kill
-screen -X -S BTS_5_4 kill
-screen -X -S BTS_5_5 kill
+screen -X -S BTS_binance kill
+screen -X -S BTS_binance kill
 screen -dmS BTS_binance  ./run.sh BTS
 
 screen -X -S DGB kill
-screen -X -S DGB_5_4 kill
-screen -X -S DGB_5_5 kill
+screen -X -S DGB_binance kill
+screen -X -S DGB_binance kill
 screen -dmS DGB_binance  ./run.sh DGB
 
 screen -X -S FLM kill
-screen -X -S FLM_5_4 kill
-screen -X -S FLM_5_5 kill
+screen -X -S FLM_binance kill
+screen -X -S FLM_binance kill
 screen -dmS FLM_binance  ./run.sh FLM
 
 screen -X -S AXS kill
