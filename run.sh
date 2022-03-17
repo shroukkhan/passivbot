0b2d0fabd977632298e307a2c68b4b10d0a61c41
Shrouk Khan <shrouk.khan@agoda.com>
2022-03-17 23:04:20 +0700

    Add analysis

--- run.sh
+++ run.sh
@@ -2,5 +2,18 @@
 cd /home/skhan/passivbot_5_5/
 source /home/skhan/passivbot_5_5/bin/activate
 echo Runing : $1 ...
-python3 passivbot.py binance_01 $1USDT cfgs_live/$1.json
+while true; do
+  python3 passivbot.py binance_01 $1USDT cfgs_live/$1.json
+  echo died..restarting in 5
+  sleep 1
+  echo died..restarting in 4
+  sleep 1
+  echo died..restarting in 3
+  sleep 1
+  echo died..restarting in 2
+  sleep 1
+  echo died..restarting in 1
+  sleep 1
+done
+
 #sleep 100
--- run_bybit.sh
+++ run_bybit.sh
@@ -2,5 +2,16 @@
 cd /home/skhan/passivbot_5_5/
 source /home/skhan/passivbot_5_5/bin/activate
 echo Runing : $1 ...
-python3 passivbot.py bybit_01 $1USDT cfgs_live_bybit/$1.json
-#sleep 100
+while true; do
+  python3 passivbot.py bybit_01 $1USDT cfgs_live_bybit/$1.json
+  echo died..restarting in 5
+  sleep 1
+  echo died..restarting in 4
+  sleep 1
+  echo died..restarting in 3
+  sleep 1
+  echo died..restarting in 2
+  sleep 1
+  echo died..restarting in 1
+  sleep 1
+done
