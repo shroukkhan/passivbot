50a9d6f46890f822557796e5400eaa74312e10a6
Shrouk Khan <shrouk.khan@agoda.com>
2022-03-25 10:08:41 +0700

    merge with master

---
+++ start_bybit.sh
@@ -0,0 +1,78 @@
+
+screen -X -S AXS_bybit kill
+screen -dmS AXS_bybit ./run_bybit.sh AXS
+
+
+screen -X -S BNB_bybit kill
+screen -dmS BNB_bybit ./run_bybit.sh BNB
+
+
+screen -X -S ATOM_bybit kill
+screen -dmS ATOM_bybit ./run_bybit.sh ATOM
+
+
+screen -X -S SAND_bybit kill
+screen -dmS SAND_bybit ./run_bybit.sh SAND
+
+screen -X -S WAVES_bybit kill
+screen -dmS WAVES_bybit ./run_bybit.sh WAVES
+
+screen -X -S DOT_bybit kill
+screen -dmS DOT_bybit ./run_bybit.sh DOT
+
+screen -X -S MATIC_bybit kill
+screen -dmS MATIC_bybit ./run_bybit.sh MATIC
+
+screen -X -S LINK_bybit kill
+screen -dmS LINK_bybit ./run_bybit.sh LINK
+
+screen -X -S LTC_bybit kill
+screen -dmS LTC_bybit ./run_bybit.sh LTC
+
+screen -X -S XTZ_bybit kill
+screen -dmS XTZ_bybit ./run_bybit.sh XTZ
+
+screen -X -S ADA_bybit kill
+screen -dmS ADA_bybit ./run_bybit.sh ADA
+
+screen -X -S XRP_bybit kill
+screen -dmS XRP_bybit ./run_bybit.sh XRP
+
+screen -X -S CHZ_bybit kill
+screen -dmS CHZ_bybit ./run_bybit.sh CHZ
+
+screen -X -S IMX_bybit kill
+screen -dmS IMX_bybit ./run_bybit.sh IMX
+
+screen -X -S RUNE_bybit kill
+screen -dmS RUNE_bybit ./run_bybit.sh RUNE
+
+screen -X -S THETA_bybit kill
+screen -dmS THETA_bybit ./run_bybit.sh THETA
+
+screen -X -S DOGE_bybit kill
+screen -dmS DOGE_bybit ./run_bybit.sh DOGE
+
+screen -X -S UNI_bybit kill
+screen -dmS UNI_bybit ./run_bybit.sh UNI
+
+screen -X -S AAVE_bybit kill
+screen -dmS AAVE_bybit ./run_bybit.sh AAVE
+
+screen -X -S ONE_bybit kill
+screen -dmS ONE_bybit ./run_bybit.sh ONE
+
+screen -X -S PEOPLE_bybit kill
+screen -dmS PEOPLE_bybit ./run_bybit.sh PEOPLE
+
+screen -X -S YFI_bybit kill
+screen -dmS YFI_bybit ./run_bybit.sh YFI
+
+screen -X -S EOS_bybit kill
+screen -dmS EOS_bybit ./run_bybit.sh EOS
+
+screen -X -S LUNA_bybit kill
+screen -dmS LUNA_bybit ./run_bybit.sh LUNA
+
+screen -X -S BCH_bybit kill
+screen -dmS BCH_bybit ./run_bybit.sh BCH
