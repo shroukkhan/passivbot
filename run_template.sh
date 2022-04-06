52860223c5102ff808d0d5af84dea917eb7d2f4b
Shrouk Khan <shrouk.khan@agoda.com>
2022-04-06 14:26:05 +0700

    adjust binance run

--- run_template.sh
+++ run_template.sh
@@ -3,7 +3,7 @@ cd /home/skhan/passivbot_5_5/
 source /home/skhan/passivbot_5_5/bin/activate
 echo Runing : $1 ...
 while true; do
-  python3 passivbot.py {command} binance_01 $1USDT ../cfgs_live/$1.json
+  python3 passivbot.py {command} binance_01 $1USDT ./cfgs_live/$1.json
   echo died..restarting in 5
   sleep 1
   echo died..restarting in 4
