c225c962683e8d45e88f1df9a156ef7fa4053c66
Shrouk Khan <shrouk.khan@agoda.com>
2023-01-02 22:32:26 +0700

    5.8 - ready for production4

--- cfgs_runtime/CHZ.sh
+++ cfgs_runtime/CHZ.sh
@@ -3,7 +3,7 @@ cd /root/passivbot_5_8/
 source /root/passivbot_5_8/.venv/bin/activate
 echo Runing : $1 ...
 while true; do
-  python3 passivbot.py -sm m -sw 0.01 -lm n -lw 0.01 binance_01 $1USDT ./cfgs_live/$1.json
+  python3 passivbot.py -sm m -sw 0.0 -lm n -lw 0.01 binance_01 $1USDT ./cfgs_live/$1.json
   echo died..restarting in 5
   sleep 1
   echo died..restarting in 4
