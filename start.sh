d0b376d5033a26d001819daac957bc3612c3de4d
Shrouk Khan <shrouk.khan@agoda.com>
2023-03-05 01:57:22 +0700

    5.9.3 - first run

--- start.sh
+++ start.sh
@@ -1,51 +1,11 @@
-screen -X -S ATOM_binance_v5_9_0 kill
-chmod +x cfgs_runtime/ATOM.sh
-screen -dmS ATOM_binance_v5_9_0 ./cfgs_runtime/ATOM.sh ATOM
+screen -X -S ETH_binance_v5_9_3_clock kill
+chmod +x cfgs_runtime/ETH.sh
+screen -dmS ETH_binance_v5_9_3_clock ./cfgs_runtime/ETH.sh ETH
 
-screen -X -S CHZ_binance_v5_9_0 kill
-chmod +x cfgs_runtime/CHZ.sh
-screen -dmS CHZ_binance_v5_9_0 ./cfgs_runtime/CHZ.sh CHZ
+screen -X -S BNB_binance_v5_9_3_clock kill
+chmod +x cfgs_runtime/BNB.sh
+screen -dmS BNB_binance_v5_9_3_clock ./cfgs_runtime/BNB.sh BNB
 
-screen -X -S DOGE_binance_v5_9_0 kill
-chmod +x cfgs_runtime/DOGE.sh
-screen -dmS DOGE_binance_v5_9_0 ./cfgs_runtime/DOGE.sh DOGE
-
-screen -X -S DOT_binance_v5_9_0 kill
-chmod +x cfgs_runtime/DOT.sh
-screen -dmS DOT_binance_v5_9_0 ./cfgs_runtime/DOT.sh DOT
-
-screen -X -S EOS_binance_v5_9_0 kill
-chmod +x cfgs_runtime/EOS.sh
-screen -dmS EOS_binance_v5_9_0 ./cfgs_runtime/EOS.sh EOS
-
-screen -X -S HBAR_binance_v5_9_0 kill
-chmod +x cfgs_runtime/HBAR.sh
-screen -dmS HBAR_binance_v5_9_0 ./cfgs_runtime/HBAR.sh HBAR
-
-screen -X -S LINK_binance_v5_9_0 kill
-chmod +x cfgs_runtime/LINK.sh
-screen -dmS LINK_binance_v5_9_0 ./cfgs_runtime/LINK.sh LINK
-
-screen -X -S MANA_binance_v5_9_0 kill
-chmod +x cfgs_runtime/MANA.sh
-screen -dmS MANA_binance_v5_9_0 ./cfgs_runtime/MANA.sh MANA
-
-screen -X -S MATIC_binance_v5_9_0 kill
-chmod +x cfgs_runtime/MATIC.sh
-screen -dmS MATIC_binance_v5_9_0 ./cfgs_runtime/MATIC.sh MATIC
-
-screen -X -S SAND_binance_v5_9_0 kill
-chmod +x cfgs_runtime/SAND.sh
-screen -dmS SAND_binance_v5_9_0 ./cfgs_runtime/SAND.sh SAND
-
-screen -X -S SUSHI_binance_v5_9_0 kill
-chmod +x cfgs_runtime/SUSHI.sh
-screen -dmS SUSHI_binance_v5_9_0 ./cfgs_runtime/SUSHI.sh SUSHI
-
-screen -X -S XRP_binance_v5_9_0 kill
-chmod +x cfgs_runtime/XRP.sh
-screen -dmS XRP_binance_v5_9_0 ./cfgs_runtime/XRP.sh XRP
-
-screen -X -S XTZ_binance_v5_9_0 kill
-chmod +x cfgs_runtime/XTZ.sh
-screen -dmS XTZ_binance_v5_9_0 ./cfgs_runtime/XTZ.sh XTZ
+screen -X -S ADA_binance_v5_9_3_clock kill
+chmod +x cfgs_runtime/ADA.sh
+screen -dmS ADA_binance_v5_9_3_clock ./cfgs_runtime/ADA.sh ADA
