cfe465146024b833c8cdaf4579625268002340ce
Shrouk Khan <shrouk.khan@agoda.com>
2023-03-06 16:25:25 +0700

    kill xmr

--- start.sh
+++ start.sh
@@ -1,7 +1,3 @@
-screen -X -S XMR_binance_v5_8 kill
-chmod +x cfgs_runtime/XMR.sh
-screen -dmS XMR_binance_v5_8 ./cfgs_runtime/XMR.sh XMR
-
 screen -X -S NEAR_binance_v5_8 kill
 chmod +x cfgs_runtime/NEAR.sh
 screen -dmS NEAR_binance_v5_8 ./cfgs_runtime/NEAR.sh NEAR
