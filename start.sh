4c901a862a903be2042f3dbb0161a02468771afd
Shrouk Khan <shrouk.khan@agoda.com>
2023-03-06 17:02:09 +0700

    Added more longs

--- start.sh
+++ start.sh
@@ -9,3 +9,23 @@ screen -dmS BNB_binance_v5_9_3_clock ./cfgs_runtime/BNB.sh BNB
 screen -X -S ADA_binance_v5_9_3_clock kill
 chmod +x cfgs_runtime/ADA.sh
 screen -dmS ADA_binance_v5_9_3_clock ./cfgs_runtime/ADA.sh ADA
+
+screen -X -S KSM_binance_v5_9_3_clock kill
+chmod +x cfgs_runtime/KSM.sh
+screen -dmS KSM_binance_v5_9_3_clock ./cfgs_runtime/KSM.sh KSM
+
+screen -X -S LTC_binance_v5_9_3_clock kill
+chmod +x cfgs_runtime/LTC.sh
+screen -dmS LTC_binance_v5_9_3_clock ./cfgs_runtime/LTC.sh LTC
+
+screen -X -S ETC_binance_v5_9_3_clock kill
+chmod +x cfgs_runtime/ETC.sh
+screen -dmS ETC_binance_v5_9_3_clock ./cfgs_runtime/ETC.sh ETC
+
+screen -X -S XMR_binance_v5_9_3_clock kill
+chmod +x cfgs_runtime/XMR.sh
+screen -dmS XMR_binance_v5_9_3_clock ./cfgs_runtime/XMR.sh XMR
+
+screen -X -S TRX_binance_v5_9_3_clock kill
+chmod +x cfgs_runtime/TRX.sh
+screen -dmS TRX_binance_v5_9_3_clock ./cfgs_runtime/TRX.sh TRX
