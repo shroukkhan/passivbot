df8af6e7dd5be69754115204f7c3ae4012a0b700
Shrouk Khan <shrouk.khan@agoda.com>
2023-04-08 09:46:09 +0700

    increase wallet size

--- cfgs_runtime/ADA.sh
+++ cfgs_runtime/ADA.sh
@@ -3,7 +3,7 @@ cd /root/passivbot_5.9.3
 source /root/passivbot_5.9.3/.venv/bin/activate
 echo Runing : $1 ...
 while true; do
-  python3 passivbot.py -sm m -sw 0 -lm n -lw 0.08 binance_01 $1USDT ./cfgs_live/$1.json
+  python3 passivbot.py -sm m -sw 0 -lm n -lw 0.1  binance_01 $1USDT ./cfgs_live/$1.json
   echo died..restarting in 5
   sleep 1
   echo died..restarting in 4
