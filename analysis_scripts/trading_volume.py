05cda35890832d190280337753f9785ba5444d2a
Shrouk Khan <shrouk.khan@agoda.com>
2023-08-19 08:55:00 +0700

    Before moving to 5.9.5

--- analysis_scripts/trading_volume.py
+++ analysis_scripts/trading_volume.py
@@ -1,45 +1,21 @@
 import requests
-from datetime import datetime, timedelta
 
-def get_trading_volume_in_usd(symbol):
-    api_url = "https://api.binance.com/api/v3/klines"
-    interval = "1d"
+def get_24h_trading_volume(symbol):
+    base_url = "https://api.binance.com/api/v3/ticker/24hr"
+    params = {"symbol": symbol}
 
-    end_time = int(datetime.now().timestamp() * 1000)
-    start_time = int((datetime.now() - timedelta(days=7)).timestamp() * 1000)
-
-    params = {
-        "symbol": symbol,
-        "interval": interval,
-        "startTime": start_time,
-        "endTime": end_time
-    }
-
-    response = requests.get(api_url, params=params)
+    response = requests.get(base_url, params=params)
 
     if response.status_code == 200:
         data = response.json()
-        trading_volume_in_usd = 0
-
-        for entry in data:
-            open_price = float(entry[1])
-            close_price = float(entry[4])
-            high_price = float(entry[2])
-            low_price = float(entry[3])
-            trading_volume = float(entry[5])
-
-            weighted_avg_price = (open_price + close_price + high_price + low_price) / 4
-            trading_volume_in_usd += weighted_avg_price * trading_volume
-
-        return trading_volume_in_usd
+        trading_volume = float(data["quoteVolume"]) / 10**6
+        return trading_volume
     else:
-        print(f"Failed to fetch data. Status code: {response.status_code}")
+        print(f"Error: Unable to fetch data. Status code: {response.status_code}")
         return None
 
-symbol = "BTCUSDT"  # Replace with the symbol you're interested in
-trading_volume_usd = get_trading_volume_in_usd(symbol)
-
-if trading_volume_usd is not None:
- print(f"Last 7 days trading volume in USD for {symbol}: {trading_volume_usd / 1000000:.2f} million")
-
-
+if __name__ == "__main__":
+    symbol = "BTCUSDT"
+    trading_volume = get_24h_trading_volume(symbol)
+    if trading_volume is not None:
+        print(f"Last 24-hour trading volume for {symbol}: {trading_volume:.2f} million USD")
