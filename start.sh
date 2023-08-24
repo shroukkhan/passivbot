dc91331b2515bf3f8813a9c6874221e4ddee5e93
Shrouk Khan <shrouk.khan@agoda.com>
2023-08-25 01:49:08 +0700

    add gs

--- start.sh
+++ start.sh
@@ -1,18 +1,6 @@
-screen -X -S ETH_binance_v5_9_3_clock kill
-chmod +x cfgs_runtime/ETH.sh
-screen -dmS ETH_binance_v5_9_3_clock ./cfgs_runtime/ETH.sh ETH
-
-screen -X -S BNB_binance_v5_9_3_clock kill
-chmod +x cfgs_runtime/BNB.sh
-screen -dmS BNB_binance_v5_9_3_clock ./cfgs_runtime/BNB.sh BNB
-
 screen -X -S ADA_binance_v5_9_3_clock kill
 chmod +x cfgs_runtime/ADA.sh
 screen -dmS ADA_binance_v5_9_3_clock ./cfgs_runtime/ADA.sh ADA
-
-screen -X -S KSM_binance_v5_9_3_clock kill
-chmod +x cfgs_runtime/KSM.sh
-screen -dmS KSM_binance_v5_9_3_clock ./cfgs_runtime/KSM.sh KSM
 
 screen -X -S LTC_binance_v5_9_3_clock kill
 chmod +x cfgs_runtime/LTC.sh
@@ -22,10 +10,6 @@ screen -X -S ETC_binance_v5_9_3_clock kill
 chmod +x cfgs_runtime/ETC.sh
 screen -dmS ETC_binance_v5_9_3_clock ./cfgs_runtime/ETC.sh ETC
 
-screen -X -S XMR_binance_v5_9_3_clock kill
-chmod +x cfgs_runtime/XMR.sh
-screen -dmS XMR_binance_v5_9_3_clock ./cfgs_runtime/XMR.sh XMR
-
 screen -X -S TRX_binance_v5_9_3_clock kill
 chmod +x cfgs_runtime/TRX.sh
 screen -dmS TRX_binance_v5_9_3_clock ./cfgs_runtime/TRX.sh TRX
@@ -34,30 +18,10 @@ screen -X -S ALGO_binance_v5_9_3_clock kill
 chmod +x cfgs_runtime/ALGO.sh
 screen -dmS ALGO_binance_v5_9_3_clock ./cfgs_runtime/ALGO.sh ALGO
 
-screen -X -S COMP_binance_v5_9_3_clock kill
-chmod +x cfgs_runtime/COMP.sh
-screen -dmS COMP_binance_v5_9_3_clock ./cfgs_runtime/COMP.sh COMP
-
 screen -X -S RUNE_binance_v5_9_3_clock kill
 chmod +x cfgs_runtime/RUNE.sh
 screen -dmS RUNE_binance_v5_9_3_clock ./cfgs_runtime/RUNE.sh RUNE
 
-screen -X -S NEAR_binance_v5_9_3_clock kill
-chmod +x cfgs_runtime/NEAR.sh
-screen -dmS NEAR_binance_v5_9_3_clock ./cfgs_runtime/NEAR.sh NEAR
-
-screen -X -S GALA_binance_v5_9_3_clock kill
-chmod +x cfgs_runtime/GALA.sh
-screen -dmS GALA_binance_v5_9_3_clock ./cfgs_runtime/GALA.sh GALA
-
-screen -X -S GMT_binance_v5_9_3_clock kill
-chmod +x cfgs_runtime/GMT.sh
-screen -dmS GMT_binance_v5_9_3_clock ./cfgs_runtime/GMT.sh GMT
-
 screen -X -S AVAX_binance_v5_9_3_clock kill
 chmod +x cfgs_runtime/AVAX.sh
 screen -dmS AVAX_binance_v5_9_3_clock ./cfgs_runtime/AVAX.sh AVAX
-
-screen -X -S BTC_binance_v5_9_3_clock kill
-chmod +x cfgs_runtime/BTC.sh
-screen -dmS BTC_binance_v5_9_3_clock ./cfgs_runtime/BTC.sh BTC
