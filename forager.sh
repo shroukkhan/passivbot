eaf2073559f7674e7068150ab8e65a3fc367f27f
Shrouk Khan <shrouk.khan@agoda.com>
2023-08-26 21:14:02 +0700

    ✨ :sparkles: Add forager.sh script for passivbot repository
    This commit adds the `forager.sh` script to the `passivbot` repository. The script is responsible for running the `forager.py` script with the appropriate configuration file. If the `forager.py` script dies, it will be automatically restarted with a countdown displayed in the terminal.

---
+++ forager.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+cd ~/passivbot
+echo Runing : python3 forager.py configs/forager/forager.hjson ...
+while true; do
+  python3 forager.py configs/forager/forager.hjson
+  echo died..restarting in 5
+  sleep 1
+  echo died..restarting in 4
+  sleep 1
+  echo died..restarting in 3
+  sleep 1
+  echo died..restarting in 2
+  sleep 1
+  echo died..restarting in 1
+  sleep 1
+done
