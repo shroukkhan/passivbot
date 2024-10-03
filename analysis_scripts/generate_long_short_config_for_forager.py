ac8866833a6441d41ab688db61f3030851339744
Shrouk Khan <shrouk.khan@agoda.com>
2024-10-03 19:35:25 +0700

    added some new config

--- analysis_scripts/generate_long_short_config_for_forager.py
+++ analysis_scripts/generate_long_short_config_for_forager.py
@@ -1,191 +1,37 @@
-longs = '''
-C:/AgodaGit/passivbot/backtests/binance/CHZUSDT/plots/2023-08-21T012902/live_config.json
-C:/AgodaGit/passivbot/backtests/binance/RSRUSDT/plots/2023-08-22T010108/live_config.json
-C:/AgodaGit/passivbot/backtests/binance/RNDRUSDT/plots/2023-08-22T004955/live_config.json
-C:/AgodaGit/passivbot/backtests/binance/HBARUSDT/plots/2023-08-21T135952/live_config.json
-C:/AgodaGit/passivbot/backtests/binance/ALPHAUSDT/plots/2023-08-22T111010/live_config.json
-C:/AgodaGit/passivbot/backtests/binance/CRVUSDT/plots/2023-08-21T205721/live_config.json
-C:/AgodaGit/passivbot/backtests/binance/ONEUSDT/plots/2023-08-22T014558/live_config.json
-C:/AgodaGit/passivbot/backtests/binance/DYDXUSDT/plots/2023-08-21T093100/live_config.json
-C:/AgodaGit/passivbot/backtests/binance/EGLDUSDT/plots/2023-08-22T083431/live_config.json
-C:/AgodaGit/passivbot/backtests/binance/XRPUSDT/plots/2023-08-22T102546/live_config.json
-C:/AgodaGit/passivbot/backtests/binance/ROSEUSDT/plots/2023-08-22T020320/live_config.json
-C:/AgodaGit/passivbot/backtests/binance/AAVEUSDT/plots/2023-08-22T080751/live_config.json
-C:/AgodaGit/passivbot/backtests/binance/ARUSDT/plots/2023-08-22T020921/live_config.json
-C:/AgodaGit/passivbot/backtests/binance/KSMUSDT/plots/2023-08-21T202639/live_config.json
-C:/AgodaGit/passivbot/backtests/binance/FTMUSDT/plots/2023-08-21T233750/live_config.json
-C:/AgodaGit/passivbot/backtests/binance/GALAUSDT/plots/2023-08-21T101736/live_config.json
-C:/AgodaGit/passivbot/backtests/binance/BAKEUSDT/plots/2023-08-21T023515/live_config.json
-C:/AgodaGit/passivbot/backtests/binance/FOOTBALLUSDT/plots/2023-08-21T094939/live_config.json
-C:/AgodaGit/passivbot/backtests/binance/ZILUSDT/plots/2023-08-22T133145/live_config.json
-C:/AgodaGit/passivbot/backtests/binance/ATOMUSDT/plots/2023-08-20T230944/live_config.json
-C:/AgodaGit/passivbot/backtests/binance/ANTUSDT/plots/2023-08-21T003218/live_config.json
-C:/AgodaGit/passivbot/backtests/binance/GRTUSDT/plots/2023-08-22T005353/live_config.json
-C:/AgodaGit/passivbot/backtests/binance/ENJUSDT/plots/2023-08-22T104636/live_config.json
-C:/AgodaGit/passivbot/backtests/binance/1000SHIBUSDT/plots/2023-08-20T224133/live_config.json
-C:/AgodaGit/passivbot/backtests/binance/BALUSDT/plots/2023-08-21T024724/live_config.json
-C:/AgodaGit/passivbot/backtests/binance/LPTUSDT/plots/2023-08-21T151103/live_config.json
-C:/AgodaGit/passivbot/backtests/binance/ASTRUSDT/plots/2023-08-21T013652/live_config.json
-C:/AgodaGit/passivbot/backtests/binance/MKRUSDT/plots/2023-08-21T165112/live_config.json
-C:/AgodaGit/passivbot/backtests/binance/DOTUSDT/plots/2023-08-21T074153/live_config.json
-C:/AgodaGit/passivbot/backtests/binance/TOMOUSDT/plots/2023-08-22T003253/live_config.json
-C:/AgodaGit/passivbot/backtests/binance/ANKRUSDT/plots/2023-08-21T002609/live_config.json
-C:/AgodaGit/passivbot/backtests/binance/MATICUSDT/plots/2023-08-21T224059/live_config.json
-C:/AgodaGit/passivbot/backtests/binance/KNCUSDT/plots/2023-08-21T140505/live_config.json
-C:/AgodaGit/passivbot/backtests/binance/RVNUSDT/plots/2023-08-21T205646/live_config.json
-C:/AgodaGit/passivbot/backtests/binance/TRUUSDT/plots/2023-08-22T004903/live_config.json
-C:/AgodaGit/passivbot/backtests/binance/IMXUSDT/plots/2023-08-21T123705/live_config.json
-C:/AgodaGit/passivbot/backtests/binance/BNBUSDT/plots/2023-08-22T065756/live_config.json
-C:/AgodaGit/passivbot/backtests/binance/ICPUSDT/plots/2023-08-21T120946/live_config.json
-C:/AgodaGit/passivbot/backtests/binance/OPUSDT/plots/2023-08-22T023439/live_config.json
-C:/AgodaGit/passivbot/backtests/binance/1000XECUSDT/plots/2023-08-20T225050/live_config.json
-C:/AgodaGit/passivbot/backtests/binance/FLOWUSDT/plots/2023-08-21T093748/live_config.json
-C:/AgodaGit/passivbot/backtests/binance/FETUSDT/plots/2023-08-22T062926/live_config.json
-C:/AgodaGit/passivbot/backtests/binance/SFPUSDT/plots/2023-08-21T212626/live_config.json
-C:/AgodaGit/passivbot/backtests/binance/API3USDT/plots/2023-08-21T005353/live_config.json
-C:/AgodaGit/passivbot/backtests/binance/MINAUSDT/plots/2023-08-21T164128/live_config.json
-C:/AgodaGit/passivbot/backtests/binance/TUSDT/plots/2023-08-22T011743/live_config.json
-C:/AgodaGit/passivbot/backtests/binance/CKBUSDT/plots/2023-08-21T053209/live_config.json
-C:/AgodaGit/passivbot/backtests/binance/NEARUSDT/plots/2023-08-21T171158/live_config.json
-C:/AgodaGit/passivbot/backtests/binance/OGNUSDT/plots/2023-08-21T175150/live_config.json
-C:/AgodaGit/passivbot/backtests/binance/MANAUSDT/plots/2023-08-21T191129/live_config.json
-C:/AgodaGit/passivbot/backtests/binance/XLMUSDT/plots/2023-08-22T025746/live_config.json
-C:/AgodaGit/passivbot/backtests/binance/NEOUSDT/plots/2023-08-21T171953/live_config.json
-C:/AgodaGit/passivbot/backtests/binance/AGIXUSDT/plots/2023-08-20T234117/live_config.json
-C:/AgodaGit/passivbot/backtests/binance/DOGEUSDT/plots/2023-08-21T044910/live_config.json
-C:/AgodaGit/passivbot/backtests/binance/DGBUSDT/plots/2023-08-21T072330/live_config.json
-C:/AgodaGit/passivbot/backtests/binance/LDOUSDT/plots/2023-08-21T143008/live_config.json
-C:/AgodaGit/passivbot/backtests/binance/QNTUSDT/plots/2023-08-21T191149/live_config.json
-C:/AgodaGit/passivbot/backtests/binance/OMGUSDT/plots/2023-08-22T063822/live_config.json
-C:/AgodaGit/passivbot/backtests/binance/PHBUSDT/plots/2023-08-21T190416/live_config.json
-C:/AgodaGit/passivbot/backtests/binance/COMPUSDT/plots/2023-08-22T100733/live_config.json
-C:/AgodaGit/passivbot/backtests/binance/HIGHUSDT/plots/2023-08-21T113756/live_config.json
-C:/AgodaGit/passivbot/backtests/binance/ARPAUSDT/plots/2023-08-21T012326/live_config.json
-C:/AgodaGit/passivbot/backtests/binance/KAVAUSDT/plots/2023-08-21T134420/live_config.json
-'''
-shorts = '''
-C:/AgodaGit/passivbot/backtests/binance/BNBUSDT/plots/2023-08-21T035448/live_config.json
-C:/AgodaGit/passivbot/backtests/binance/AUDIOUSDT/plots/2023-08-21T020433/live_config.json
-C:/AgodaGit/passivbot/backtests/binance/UNFIUSDT/plots/2023-08-22T013345/live_config.json
-C:/AgodaGit/passivbot/backtests/binance/SUSHIUSDT/plots/2023-08-22T063819/live_config.json
-C:/AgodaGit/passivbot/backtests/binance/ANTUSDT/plots/2023-08-21T003920/live_config.json
-C:/AgodaGit/passivbot/backtests/binance/ATOMUSDT/plots/2023-08-21T015621/live_config.json
-C:/AgodaGit/passivbot/backtests/binance/JASMYUSDT/plots/2023-08-21T132651/live_config.json
-C:/AgodaGit/passivbot/backtests/binance/OCEANUSDT/plots/2023-08-21T174020/live_config.json
-C:/AgodaGit/passivbot/backtests/binance/XMRUSDT/plots/2023-08-22T063107/live_config.json
-C:/AgodaGit/passivbot/backtests/binance/LUNA2USDT/plots/2023-08-21T155135/live_config.json
-C:/AgodaGit/passivbot/backtests/binance/ZECUSDT/plots/2023-08-22T102333/live_config.json
-C:/AgodaGit/passivbot/backtests/binance/APEUSDT/plots/2023-08-21T004134/live_config.json
-C:/AgodaGit/passivbot/backtests/binance/DOGEUSDT/plots/2023-08-21T073137/live_config.json
-C:/AgodaGit/passivbot/backtests/binance/BNXUSDT/plots/2023-08-21T040400/live_config.json
-C:/AgodaGit/passivbot/backtests/binance/BALUSDT/plots/2023-08-21T024428/live_config.json
-C:/AgodaGit/passivbot/backtests/binance/ZENUSDT/plots/2023-08-22T103855/live_config.json
-C:/AgodaGit/passivbot/backtests/binance/GMXUSDT/plots/2023-08-21T104854/live_config.json
-C:/AgodaGit/passivbot/backtests/binance/1000LUNCUSDT/plots/2023-08-20T223524/live_config.json
-C:/AgodaGit/passivbot/backtests/binance/DARUSDT/plots/2023-08-21T064347/live_config.json
-C:/AgodaGit/passivbot/backtests/binance/KSMUSDT/plots/2023-08-21T141322/live_config.json
-C:/AgodaGit/passivbot/backtests/binance/ZILUSDT/plots/2023-08-22T104733/live_config.json
-C:/AgodaGit/passivbot/backtests/binance/VETUSDT/plots/2023-08-22T020516/live_config.json
-C:/AgodaGit/passivbot/backtests/binance/ACHUSDT/plots/2023-08-20T231826/live_config.json
-C:/AgodaGit/passivbot/backtests/binance/ENJUSDT/plots/2023-08-21T082127/live_config.json
-C:/AgodaGit/passivbot/backtests/binance/CKBUSDT/plots/2023-08-21T052900/live_config.json
-C:/AgodaGit/passivbot/backtests/binance/IOTXUSDT/plots/2023-08-21T131856/live_config.json
-C:/AgodaGit/passivbot/backtests/binance/CTKUSDT/plots/2023-08-21T061409/live_config.json
-C:/AgodaGit/passivbot/backtests/binance/SANDUSDT/plots/2023-08-21T210918/live_config.json
-C:/AgodaGit/passivbot/backtests/binance/BANDUSDT/plots/2023-08-21T025032/live_config.json
-C:/AgodaGit/passivbot/backtests/binance/ONEUSDT/plots/2023-08-21T181043/live_config.json
-C:/AgodaGit/passivbot/backtests/binance/ARUSDT/plots/2023-08-21T013145/live_config.json
-C:/AgodaGit/passivbot/backtests/binance/MATICUSDT/plots/2023-08-21T163144/live_config.json
-C:/AgodaGit/passivbot/backtests/binance/PERPUSDT/plots/2023-08-21T185334/live_config.json
-C:/AgodaGit/passivbot/backtests/binance/NEARUSDT/plots/2023-08-21T171158/live_config.json
-C:/AgodaGit/passivbot/backtests/binance/BLUEBIRDUSDT/plots/2023-08-21T032724/live_config.json
-C:/AgodaGit/passivbot/backtests/binance/RLCUSDT/plots/2023-08-21T200028/live_config.json
-C:/AgodaGit/passivbot/backtests/binance/AGIXUSDT/plots/2023-08-20T234014/live_config.json
-C:/AgodaGit/passivbot/backtests/binance/ANKRUSDT/plots/2023-08-21T002506/live_config.json
-C:/AgodaGit/passivbot/backtests/binance/1000SHIBUSDT/plots/2023-08-20T224235/live_config.json
-C:/AgodaGit/passivbot/backtests/binance/HOTUSDT/plots/2023-08-21T115638/live_config.json
-C:/AgodaGit/passivbot/backtests/binance/GALUSDT/plots/2023-08-21T102905/live_config.json
-C:/AgodaGit/passivbot/backtests/binance/GALAUSDT/plots/2023-08-21T101736/live_config.json
-C:/AgodaGit/passivbot/backtests/binance/REEFUSDT/plots/2023-08-21T194117/live_config.json
-C:/AgodaGit/passivbot/backtests/binance/ASTRUSDT/plots/2023-08-21T014258/live_config.json
-C:/AgodaGit/passivbot/backtests/binance/RVNUSDT/plots/2023-08-21T205935/live_config.json
-C:/AgodaGit/passivbot/backtests/binance/DOTUSDT/plots/2023-08-21T074051/live_config.json
-C:/AgodaGit/passivbot/backtests/binance/TRBUSDT/plots/2023-08-22T004030/live_config.json
-C:/AgodaGit/passivbot/backtests/binance/DASHUSDT/plots/2023-08-21T065501/live_config.json
-C:/AgodaGit/passivbot/backtests/binance/CHRUSDT/plots/2023-08-21T051132/live_config.json
-C:/AgodaGit/passivbot/backtests/binance/PEOPLEUSDT/plots/2023-08-21T184025/live_config.json
-C:/AgodaGit/passivbot/backtests/binance/TRBUSDT/plots/2023-08-22T003906/live_config.json
-C:/AgodaGit/passivbot/backtests/binance/DGBUSDT/plots/2023-08-21T072228/live_config.json
-C:/AgodaGit/passivbot/backtests/binance/SKLUSDT/plots/2023-08-21T213637/live_config.json
-C:/AgodaGit/passivbot/backtests/binance/CHZUSDT/plots/2023-08-21T051838/live_config.json
-C:/AgodaGit/passivbot/backtests/binance/ENSUSDT/plots/2023-08-21T083348/live_config.json
-C:/AgodaGit/passivbot/backtests/binance/DEFIUSDT/plots/2023-08-21T070207/live_config.json
-C:/AgodaGit/passivbot/backtests/binance/FXSUSDT/plots/2023-08-21T100652/live_config.json
-C:/AgodaGit/passivbot/backtests/binance/COMPUSDT/plots/2023-08-21T054834/live_config.json
-'''
-
-import os
-import shutil
-
-
-def get_symbol_from_file(config_file):
-    symbol = config_file.split('/')[-4]
-    return symbol
-
-
-def copy_file_to_directory(config_file, directory):
-    src = config_file
-    dst = directory + '/' + get_symbol_from_file(config_file) + '.json'
-    if os.path.isfile(src):
-        shutil.copyfile(src, dst)
-
-
-def copy_files(text, directory):
-    for line in text.split('\n'):
-        if line:
-            line = line.replace('\\', '/')
-            copy_file_to_directory(line, directory)
-
-
-def get_output_from_files(text, longs_or_shorts):
-    outputs = []
-    for line in text.split('\n'):
-        if line:
-            line = line.replace('\\', '/')
-            outputs.append(
-                f'{get_symbol_from_file(line)}: configs/live/scud2/{longs_or_shorts}/{get_symbol_from_file(line)}.json')
-    return outputs
-
-
-def delete_files(directory):
-    # delete all json files in folder
-    for file in os.listdir(directory):
-        if file.endswith('.json'):
-            os.remove(f'{directory}/{file}')
-
-
 # write main method like ___main___ in python
+import os
+
+
 if __name__ == '__main__':
+    directories = {'longs': r'C:\AgodaGit\passivbot\configs\live\longs',
+                   'shorts': r'C:\AgodaGit\passivbot\configs\live\shorts'}
+    ROOT_PATH = 'C:/AgodaGit/passivbot/'
+    
+    '''
+    now 
+    1. iterate through longs and shorts directories listing all files ending with .json , use glob
+    2. for each file , get the name without extension
+    3. append the name and the file path starting from configs/live/longs/ or configs/live/shorts/ to two separate list
+    '''
+    
+    longs = []
+    shorts = []
+    for directory in directories:
+        for file in os.listdir(directories[directory]):
+            if file.endswith('.json'):
+                file_name_without_extension = file.split('.')[0]
+                _directory = directories[directory].replace('\\', '/').replace(ROOT_PATH,'')
+                #line_to_append = f'{file_name_without_extension}: {_directory}/{file}'
+                line_to_append = f'{file_name_without_extension}'
+                if directory == 'longs':
+                    longs.append(line_to_append)
+                else:
+                    shorts.append(line_to_append)
 
-    # create a dictionary for longs and shorts directories
-    directories = {'longs': r'C:\AgodaGit\passivbot\configs\live\scud2\longs',
-                   'shorts': r'C:\AgodaGit\passivbot\configs\live\scud2\shorts'}
-
-    outputs = {'longs': [], 'shorts': []}
-
-    # iterate over key value of directories
-    for key, value in directories.items():
-        # delete files in directory
-        delete_files(value)
-        # copy files to directory
-        if key == 'longs':
-            copy_files(longs, value)
-            outputs['longs'] = get_output_from_files(longs, 'longs')
-        else:
-            copy_files(shorts, value)
-            outputs['shorts'] = get_output_from_files(shorts, 'shorts')
 
     # print longs as a string separated by new line
-    print('\n'.join(outputs['longs']))
+    print('\n'.join(longs))
     print('\n\n-----\n\n')
-    print('\n'.join(outputs['shorts']))
+    print('\n'.join(shorts))
+    
+
